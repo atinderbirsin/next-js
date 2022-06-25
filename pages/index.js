d2391d59f7f764674e275bc0405fa1aad8936225
Atinderbir Singh <atinderbir3@gmail.com>
2022-06-26 01:36:02 +0530

    Link component navigation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 function Home() {
-    return <h1>Home Page</h1>
-};
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Link href="/blog">
+        <a>blog</a>
+      </Link>
+      <Link href="/product">
+        <a>product</a>
+      </Link>
+    </>
+  );
+}
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,11 +1,35 @@
+import Link from 'next/link';
+
 function Product() {
+  const createLink = (id) => {
     return (
-        <>
-            <h2>Product 1</h2>
-            <h2>Product 2</h2>
-            <h2>Product 3</h2>
-        </>
-    )
-};
+      <Link href={`/product/${id}`}>
+        <a>Product {id}</a>
+      </Link>
+    );
+  };
+
+  return (
+    <>
+      <h2>
+        <Link href="/product/1">
+          <a>Product 1</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href="/product/2">
+          <a>Product 2</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href="/product/3">
+          <a>Product 3</a>
+        </Link>
+      </h2>
+      <h2>{createLink(100)}</h2>
+      <h2>{createLink(55)}</h2>
+    </>
+  );
+}
 
 export default Product;

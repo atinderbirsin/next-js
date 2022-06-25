707bba13eb43c1624751832bee834a6611d6f407
Atinderbir Singh <atinderbir3@gmail.com>
2022-06-26 01:48:40 +0530

    Implemented programmatical navigation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
-import Link from "next/link";
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 function Home() {
+  const router = useRouter();
+
+  const onButtonClick = (link) => {
+    console.log('placing your order!.');
+    router.push(link);
+  };
+
   return (
     <>
       <h1>Home Page</h1>
@@ -10,6 +18,8 @@ function Home() {
       <Link href="/product">
         <a>product</a>
       </Link>
+      <button onClick={() => onButtonClick('/about')}>Place Order</button>
+      <button onClick={() => onButtonClick('/product/44')}>Place Order</button>
     </>
   );
 }
--- pages/product/index.js
+++ pages/product/index.js
@@ -22,7 +22,7 @@ function Product() {
         </Link>
       </h2>
       <h2>
-        <Link href="/product/3">
+        <Link href="/product/3" replace>
           <a>Product 3</a>
         </Link>
       </h2>

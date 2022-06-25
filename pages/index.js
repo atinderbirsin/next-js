2a9bdb9b2e72390ad7e4559c75dc4493ed2a32fe
Atinderbir Singh <atinderbir3@gmail.com>
2022-06-26 01:52:56 +0530

    Implemented programmatical navigation and replace method

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-import Link from "next/link";
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 function Home() {
+  const router = useRouter();
+
+  const onButtonClick = (link) => {
+    router.push(link);
+  };
+
   return (
     <>
       <h1>Home Page</h1>
@@ -10,6 +17,9 @@ function Home() {
       <Link href="/product">
         <a>product</a>
       </Link>
+      <button onClick={() => onButtonClick('/about')}>Place Order</button>
+
+      <button onClick={() => onButtonClick('/product/44')}>Place Order</button>
     </>
   );
 }
